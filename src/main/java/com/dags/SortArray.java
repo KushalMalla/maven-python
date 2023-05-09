bb0bac90d07c166a58c7674c9faee423b3243735
KushalMalla <kushal.malla@tcs.com>
2023-05-09 14:53:39 +0000

    change in code

--- src/main/java/com/dags/SortArray.java
+++ src/main/java/com/dags/SortArray.java
@@ -1,5 +1,5 @@
 public class SortArray{
-    public void sortArray(int[] array) {
+    public int sortArray(int[] array) {
         int n = array.length;
         for (int i = 0; i < n-1; i++) {
             for (int j = 0; j < n-i-1; j++) {
